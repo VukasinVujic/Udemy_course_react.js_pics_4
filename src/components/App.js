a6b91349cb6edad2eb18a8aca483900d40f82a27
VukasinVujic <vukasinvujic021@gmail.com>
2020-03-12 10:46:29 +0100

    adding then to promise and adding alter way with async & await

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,17 @@ import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
-    onSearchSubmit(term){
-        axios.get('https://api.unsplash.com/search/photos', {
+    async onSearchSubmit(term){
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term},
             headers: {
                 Authorization: 'Client-ID g7BII5uIP7Ke83-h4wvLn_66BGoGW-vqAzqa0l_RSA4'
             }
         })
+        // .then((response)=>{
+        //     console.log(response.data.results)
+        // })
+        console.log(response.data.results);  
     }
 
     render(){
